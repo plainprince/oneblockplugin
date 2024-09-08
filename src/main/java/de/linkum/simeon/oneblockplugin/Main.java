c5be055dafd2b82d8904c405562ac2c1d8d7fa35
Simeon Kummer <simeon@hitthecode.de>
2024-09-08 10:35:18 +0200

    Fixed SonarLint issues

--- src/main/java/de/linkum/simeon/oneblockplugin/Main.java
+++ src/main/java/de/linkum/simeon/oneblockplugin/Main.java
@@ -4,7 +4,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.WorldCreator;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.event.player.PlayerJoinEvent;
 
 public class Main extends JavaPlugin {
 
@@ -22,8 +21,6 @@ public class Main extends JavaPlugin {
             World newWorld = Bukkit.createWorld(new WorldCreator(dimension));
             newWorld.setSpawnLocation(0, 101, 0);
         }
-
-        Bukkit.getLogger().info(String.valueOf(Bukkit.getWorld(dimension)));
 
         getServer().getPluginManager().registerEvents(new BlockListener(this, dimension), this);
     }
