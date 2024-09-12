e27b6534d8819b99709e3325a46769a7eb1793f5
Simeon Kummer <simeon@hitthecode.de>
2024-09-12 16:35:18 +0200

    fixed all bugs (except friend-stuff, i'm sitll trying to fix it) and changed a bit of stuff

--- src/main/java/de/linkum/simeon/oneblockplugin/BlockListener.java
+++ src/main/java/de/linkum/simeon/oneblockplugin/BlockListener.java
@@ -8,46 +8,73 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.world.ChunkLoadEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scoreboard.*;
 import org.jetbrains.annotations.NotNull;
 
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 import static org.bukkit.Material.*;
 
 public class BlockListener implements Listener {
-
     private final Main plugin;
     private final Location oneBlockLocation;
     private int stage = 1;
     private int highestStage = 1;
     private int blocksBroken = 0;
     private final Random random = new Random();
+    private FriendSystem friendSystem;
+    private String playerName;
+    private int blocksBrokenAllTime = 0;
 
     public BlockListener(Main plugin, String worldName) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                Player player = Bukkit.getPlayer(playerName);
+                assert player != null;
+
+                if(player.getWorld().getName().startsWith("customdimension")) {
+                    updateScoreboard(player);
+                }
+            }
+        }.runTaskTimer(plugin, 0L, 2L);
+
         this.plugin = plugin;
         World world = Bukkit.getWorld(worldName);
         this.oneBlockLocation = new Location(world, 0, 100, 0); // Setze hier die Koordinaten des OneBlock
         this.spawnNextBlock(oneBlockLocation.getBlock());
+        this.playerName = worldName.substring(15);
+        this.friendSystem = new FriendSystem(plugin, playerName);
 
-        Command command = new Command("oneblockstage") {
+        Command command = new Command("stage") {
             @Override
             public boolean execute(@NotNull CommandSender sender, @NotNull String label, String[] args) {
                 if (sender instanceof Player && args.length == 1) {
+                    if(!args[0].matches("-?\\d+")) {
+                        sender.sendMessage(args[0] + " ist keine Zahl, bitte wähle eine stage größer als 0.");
+                        return true;
+                    }
+                    if(!(Integer.parseInt(args[0]) > 0)) {
+                        sender.sendMessage("Bitte wähle eine stage größer als 0.");
+                        return true;
+                    }
                     if(Integer.parseInt(args[0]) <= highestStage) {
                         stage = Integer.parseInt(args[0]);
                         blocksBroken = 0;
-                        sender.sendMessage("set stage to: " + stage);
+                        sender.sendMessage("Hat die stage zu: " + stage + " gesetzt");
                     } else {
-                        sender.sendMessage("stage not unlocked yet");
+                        sender.sendMessage("Du musst zuerst noch Blöcke abbauen!");
                     }
                 }
                 return true;
             }
         };
 
-        this.plugin.getServer().getCommandMap().register("oneblockstage", command);
+        this.plugin.getServer().getCommandMap().register("oneblock", command);
     }
 
     @EventHandler
@@ -55,14 +82,26 @@ public class BlockListener implements Listener {
         Player player = event.getPlayer();
         Block block = event.getBlock();
 
-        if (block.getLocation().getX() != oneBlockLocation.getX() ||
-                block.getLocation().getY() != oneBlockLocation.getY() ||
-                block.getLocation().getZ() != oneBlockLocation.getZ() ||
-                !block.getWorld().getName().equals(oneBlockLocation.getWorld().getName())) {
+        if (!block.getWorld().getName().equals(oneBlockLocation.getWorld().getName())) {
             return;
         }
-        blocksBroken++;
 
+        Bukkit.getLogger().info(String.valueOf(friendSystem.getFriends()));
+
+        if(!player.getName().equals(playerName) && (friendSystem.getFriends() == null || !friendSystem.getFriends().contains(player.getName()))) {
+            event.setCancelled(true);
+            player.sendMessage("Hey! You can't break that here!");
+            return;
+        }
+
+        if(block.getLocation().getX() != oneBlockLocation.getX() ||
+                block.getLocation().getY() != oneBlockLocation.getY() ||
+                block.getLocation().getZ() != oneBlockLocation.getZ()) {
+            return;
+        }
+
+        blocksBroken++;
+        blocksBrokenAllTime++;
         player.giveExp(event.getExpToDrop());
         Collection<ItemStack> drops = block.getDrops(player.getInventory().getItemInMainHand());
 
@@ -82,10 +121,67 @@ public class BlockListener implements Listener {
 
         event.setDropItems(false);
         Bukkit.getScheduler().scheduleSyncDelayedTask(this.plugin, () -> this.spawnNextBlock(block), 1L);
-        if(blocksBroken > (100 * stage) - 1) {
+        if(blocksBroken > (400 * stage) - 1) {
             blocksBroken = 0;
             stage++;
-            highestStage++;
+            if(stage > highestStage) {
+                highestStage++;
+                player.sendTitle("Neue Stage: " + highestStage, null, 20, 60, 20);
+            }
+        }
+    }
+
+    private void updateScoreboard(Player player) {
+        ScoreboardManager manager = Bukkit.getScoreboardManager();
+        Scoreboard board = manager.getNewScoreboard();
+        Objective objective = board.registerNewObjective("BlitzGamer", "dummy", ChatColor.GOLD + "BlitzGamer.javnet.de");
+        objective.setDisplaySlot(org.bukkit.scoreboard.DisplaySlot.SIDEBAR);
+
+        String dateTime = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(new Date());
+
+        Score textScore = objective.getScore(ChatColor.GREEN + "Der beste Server auf der Welt!");
+        textScore.setScore(7);
+
+        Score dateScore = objective.getScore(ChatColor.WHITE + "Datum & Uhrzeit: " + dateTime);
+        dateScore.setScore(6);
+
+        Score nameScore = objective.getScore(ChatColor.WHITE + "Dein Name: " + player.getName());
+        nameScore.setScore(5);
+
+        int onlinePlayers = Bukkit.getOnlinePlayers().size();
+        Score onlineScore = objective.getScore(ChatColor.WHITE + "Online Spieler: " + onlinePlayers);
+        onlineScore.setScore(4);
+
+        Score stageScore = objective.getScore(ChatColor.WHITE + "Stage: " + stage);
+        stageScore.setScore(3);
+
+        Score highestStageScore = objective.getScore(ChatColor.WHITE + "Höchste stage: " + highestStage);
+        highestStageScore.setScore(2);
+
+        Score blocksBrokenScore = objective.getScore(ChatColor.WHITE + "OneBlocks zerstört: " + blocksBrokenAllTime);
+        blocksBrokenScore.setScore(1);
+
+        int currentStageBlocks = 400 * stage;
+        Score blocksTillNextStage = objective.getScore(ChatColor.WHITE + "Blöcke bis zur nächsten stage: " + (currentStageBlocks - blocksBroken));
+        blocksTillNextStage.setScore(0);
+
+        player.setScoreboard(board);
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+
+        if(!player.getWorld().getName().equals(oneBlockLocation.getWorld().getName())) {
+            return;
+        }
+
+        if(!player.getName().equals(playerName) && (friendSystem.getFriends() == null || !friendSystem.getFriends().contains(player.getName()))) {
+            event.setCancelled(true);
+            if(event.getClickedBlock() == null) {
+                return;
+            }
+            player.sendMessage(ChatColor.RED + "Hey! Du kannst das hier nicht machen!");
         }
     }
 
@@ -109,36 +205,18 @@ public class BlockListener implements Listener {
         }
     }
 
-    private Material getSurfaceBlock() {
-        Material[] surfaceBlocks = {GRASS_BLOCK, OAK_LOG, BIRCH_LOG};
-        return surfaceBlocks[random.nextInt(surfaceBlocks.length)];
-    }
-
-    private Material getUndergroundBlock() {
-        Material[] undergroundBlocks = {Material.STONE, Material.IRON_ORE, GOLD_ORE, DIAMOND_ORE, LAPIS_ORE};
-        return undergroundBlocks[random.nextInt(undergroundBlocks.length)];
-    }
-
-    private Material getNetherBlock() {
-        Material[] netherBlocks = {Material.NETHERRACK, Material.NETHER_BRICKS, Material.NETHER_QUARTZ_ORE};
-        return netherBlocks[random.nextInt(netherBlocks.length)];
-    }
-
-    @EventHandler
-    public void onChunkLoad(ChunkLoadEvent event) {
-        if(event.getWorld().getName().startsWith("customdimension")) {
-            for( int x = 0; x < 16; x++ ) {
-                for (int y = -64; y < 320; y++) {
-                    for (int z = 0; z < 16; z++) {
-                        Block block = event.getChunk().getBlock(x, y, z);
-                        block.setType(Material.AIR);
-                    }
-                }
-            }
+        private Material getSurfaceBlock() {
+            Material[] surfaceBlocks = {GRASS_BLOCK, OAK_LOG, BIRCH_LOG};
+            return surfaceBlocks[random.nextInt(surfaceBlocks.length)];
         }
-        if(event.isNewChunk()) {
-            Block b = event.getWorld().getBlockAt(0, 100, 0);
-            b.setType(GRASS_BLOCK);
+
+        private Material getUndergroundBlock() {
+            Material[] undergroundBlocks = {Material.STONE, Material.IRON_ORE, GOLD_ORE, DIAMOND_ORE, LAPIS_ORE};
+            return undergroundBlocks[random.nextInt(undergroundBlocks.length)];
         }
-    }
+
+        private Material getNetherBlock() {
+            Material[] netherBlocks = {Material.NETHERRACK, Material.NETHER_BRICKS, Material.NETHER_QUARTZ_ORE};
+            return netherBlocks[random.nextInt(netherBlocks.length)];
+        }
 }
--- src/main/java/de/linkum/simeon/oneblockplugin/Main.java
+++ src/main/java/de/linkum/simeon/oneblockplugin/Main.java
@@ -5,88 +5,149 @@ import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
+import java.util.concurrent.TimeUnit;
 
 public class Main extends JavaPlugin {
+    public Plugin worldguard = getServer().getPluginManager().getPlugin("worldguard");
 
     @Override
     public void onEnable() {
+        getLogger().info(String.valueOf(worldguard));
         Bukkit.getWorlds().forEach(i -> {
-            Bukkit.getLogger().info("World: " + i.getName());
-            Bukkit.getLogger().info("OneBlockWorld?: " + i.getName().startsWith("customdimension"));
+            getLogger().info("World: " + i.getName());
+            getLogger().info("OneBlockWorld?: " + i.getName().startsWith("customdimension"));
             if(i.getName().startsWith("customdimension")) {
                 createListenerInDimension(i.getName());
             }
+            getLogger().info("");
         });
         getLogger().info("OneBlock Plugin enabled!");
 
         // Register the custom command with Bukkit
-        Command command = new Command("oneblock") {
+        Command command = new Command("create") {
             @Override
             public boolean execute(@NotNull CommandSender sender, @NotNull String label, String[] args) {
-                if (sender instanceof Player player && args.length > 0) {
+                if (sender instanceof Player player && args.length == 0) {
                     String dimensionName = "customdimension" + player.getName();
-                    if(args[0].equals("create")) {
-                        if(Bukkit.getWorld(dimensionName) != null) {
-                            player.sendMessage("you already have a OneBlock, to go to your OneBlock run /oneblock tp");
-                        }else {
-                            player.sendMessage("building oneBlock");
-                            Bukkit.getLogger().info("building oneBlock");
-                            createListenerInDimension(dimensionName);
-                            player.sendMessage("done building oneBlock");
-                            Location tpLocation = new Location(Bukkit.getWorld(dimensionName), 0.5, 101, 0.5);
-                            player.sendMessage("teleporting...");
-                            player.teleport(tpLocation);
-                        }
-                    }
-                    if(args[0].equals("tp")) {
-                        if(Bukkit.getWorld(dimensionName) != null) {
-                            Location tpLocation = new Location(Bukkit.getWorld(dimensionName), 0.5, 101, 0.5);
-                            player.sendMessage("teleporting...");
-                            player.teleport(tpLocation);
-                        }else {
-                            player.sendMessage("you don't have a OneBlock yet, run /oneblock create first");
-                        }
-                    }
-                    if(args[0].equals("clear")) {
-                        player.sendMessage("This will delete your entire Island. If you really want to do this run /oneblock forceClear");
-                    }
-                    if(args[0].equals("forceClear")) {
-                        for (int i = -32; i < 32; i++) {
-                            for (int j = -64; j < 320; j++) {
-                                for (int k = -32; k < 32; k++) {
-                                    Block block = Bukkit.getWorld(dimensionName).getBlockAt(i, j, k);
-                                    if(block.getLocation().equals(new Location(Bukkit.getWorld(dimensionName), 0, 100, 0))) {
-                                        break;
-                                    }
-                                    block.setType(Material.AIR);
-                                }
-                            }
-                        }
-                    }
-                    if(args[0].equals("addFriend")) {
-
-                    }
-                    if(args[0].equals("removeFriend")) {
-
+                    if(Bukkit.getWorld(dimensionName) != null) {
+                        player.sendMessage("you already have a OneBlock, to go to your OneBlock run /oneblock tp");
+                    }else {
+                        player.sendMessage("building oneBlock");
+                        getLogger().info("building oneBlock");
+                        createListenerInDimension(dimensionName);
+                        player.sendMessage("done building oneBlock");
+                        Location tpLocation = new Location(Bukkit.getWorld(dimensionName), 0.5, 101, 0.5);
+                        player.sendMessage("teleporting...");
+                        player.teleport(tpLocation);
                     }
                 } else {
-                    sender.sendMessage("Incorrect usage of command. use /oneblock [create, tp, clear, addFriend, removeFriend] or /oneblockstage <stage>");
+                    sender.sendMessage("Incorrect usage of command. use /oneblock:[create, tp, clear, addFriend, removeFriend, stage]");
                 }
                 return true; // Indicates that the command has been handled by this executor.
             }
         };
 
         this.getServer().getCommandMap().register("oneblock", command);
+
+        command = new Command("tp") {
+            @Override
+            public boolean execute(@NotNull CommandSender sender, @NotNull String label, String[] args) {
+                if (sender instanceof Player player && args.length == 0) {
+                    String dimensionName = "customdimension" + player.getName();
+                    if (Bukkit.getWorld(dimensionName) != null) {
+                        Location tpLocation = new Location(Bukkit.getWorld(dimensionName), 0.5, 101, 0.5);
+                        player.sendMessage("teleporting...");
+                        player.teleport(tpLocation);
+                    } else {
+                        player.sendMessage("you don't have a OneBlock yet, run /oneblock:create first");
+                    }
+                }
+
+                if (sender instanceof Player player && args.length == 1) {
+                    String dimensionName = "customdimension" + args[0];
+                    if (Bukkit.getWorld(dimensionName) != null) {
+                        Location tpLocation = new Location(Bukkit.getWorld(dimensionName), 0.5, 101, 0.5);
+                        player.sendMessage("teleporting...");
+                        player.teleport(tpLocation);
+                    } else {
+                        player.sendMessage("this oneblock does not exist");
+                    }
+                }
+                return true;
+            }
+        };
+
+        this.getServer().getCommandMap().register("oneblock", command);
+
+        command = new Command("clear") {
+            @Override
+            public boolean execute(@NotNull CommandSender sender, @NotNull String label, String[] args) {
+                if (sender instanceof Player player && args.length == 0) {
+                    player.sendMessage("This will delete your entire Island. If you really want to do this run /oneblock:forceClear");
+                }
+                return true;
+            }
+        };
+
+        this.getServer().getCommandMap().register("oneblock", command);
+
+        command = new Command("forceClear") {
+            @Override
+            public boolean execute(@NotNull CommandSender sender, @NotNull String label, String[] args) {
+                if (sender instanceof Player player && args.length == 0) {
+                    String dimensionName = "customdimension" + player.getName();
+                    player.sendMessage("starting to clear island");
+                    for (int i = -32; i < 32; i++) {
+                        for (int j = -64; j < 320; j++) {
+                            for (int k = -32; k < 32; k++) {
+                                Block block = Bukkit.getWorld(dimensionName).getBlockAt(i, j, k);
+                                block.setType(Material.AIR);
+                            }
+                        }
+                    }
+                    Bukkit.getWorld(dimensionName).getBlockAt(0, 100, 0).setType(Material.OAK_LOG);
+                    player.sendMessage("cleared island");
+                }
+                return true;
+            }
+        };
+
+        this.getServer().getCommandMap().register("oneblock", command);
+
+        new FriendSystem(this, "thisplayerdoesnotexist");
     }
 
     public void createListenerInDimension(String dimension) {
         if (Bukkit.getWorld(dimension) == null) {
-            World newWorld = Bukkit.createWorld(new WorldCreator(dimension));
+            WorldCreator worldCreator = new WorldCreator(dimension);
+            worldCreator.type(WorldType.FLAT);
+            worldCreator.generateStructures(false);
+            World newWorld = Bukkit.createWorld(worldCreator);
             newWorld.setSpawnLocation(0, 101, 0);
-            newWorld.getWorldBorder().setCenter(0, 0);
-            newWorld.getWorldBorder().setSize(64);
+            newWorld.getWorldBorder().setCenter(0.5, 0.5);
+            newWorld.getWorldBorder().setSize(65);
+
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+
+            for (int i = -32; i < 33; i++) {
+                for (int j = -64; j < -60; j++) {
+                    for (int k = -32; k < 33; k++) {
+                        Block block = Bukkit.getWorld(dimension).getBlockAt(i, j, k);
+                        block.setType(Material.AIR);
+                    }
+                }
+            }
+
+            Block block = Bukkit.getWorld(dimension).getBlockAt(0, 100, 0);
+            block.setType(Material.OAK_LOG);
         }
 
         getServer().getPluginManager().registerEvents(new BlockListener(this, dimension), this);
